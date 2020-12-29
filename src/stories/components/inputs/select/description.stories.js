5e35aaf62b5daf79c845eb753b0acc63bfd43b2b
Ngadiono CR <ngadionocr@gmail.com>
2020-12-29 21:08:23 +0700

    adding select submenu

--- src/stories/components/inputs/select/description.stories.js
+++ src/stories/components/inputs/select/description.stories.js
@@ -4,8 +4,11 @@ import { withDesign } from 'storybook-addon-designs';
 
 // Material UI
 import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import MenuItem from '@material-ui/core/MenuItem';
 
 // Components
+import InputLabel from 'components/inputs/InputLabel';
 import Select from 'components/inputs/Select';
 import SelectDescriptionDocs from './description.docs.mdx';
 
@@ -33,7 +36,22 @@ export default {
 };
 export const description = () => (
   <Wrapper>
-    <Select />
+    <FormControl variant="outlined" style={{ minWidth: 150 }}>
+      <InputLabel>Simple Select</InputLabel>
+      <Select
+        label="Simple Select"
+        inputProps={{
+          name: 'simple-select',
+        }}
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+    </FormControl>
   </Wrapper>
 );
 
--- src/stories/components/inputs/select/example.stories.js
+++ src/stories/components/inputs/select/example.stories.js
@@ -9,6 +9,15 @@ import { makeStyles } from '@material-ui/core/styles';
 import SelectTypes from './select-types';
 import SelectTypesDocs from './select-types/docs.mdx';
 
+import SelectFormProps from './select-form-props';
+import SelectFormPropsDocs from './select-form-props/docs.mdx';
+
+import SelectMultiple from './select-multiple';
+import SelectMultipleDocs from './select-multiple/docs.mdx';
+
+import SelectGrouping from './select-grouping';
+import SelectGroupingDocs from './select-grouping/docs.mdx';
+
 import 'font-family.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -31,17 +40,59 @@ export default {
     design: { disabled: true },
   },
 };
+
 export const types = () => (
   <Wrapper>
     <SelectTypes />
   </Wrapper>
 );
-
 types.story = {
   parameters: {
     status: 'Development', // Stable | Development | Deprecated
     docs: {
       page: SelectTypesDocs,
+    },
+  },
+};
+
+export const formProps = () => (
+  <Wrapper>
+    <SelectFormProps />
+  </Wrapper>
+);
+formProps.story = {
+  parameters: {
+    status: 'Development', // Stable | Development | Deprecated
+    docs: {
+      page: SelectFormPropsDocs,
+    },
+  },
+};
+
+export const multiple = () => (
+  <Wrapper>
+    <SelectMultiple />
+  </Wrapper>
+);
+multiple.story = {
+  parameters: {
+    status: 'Development', // Stable | Development | Deprecated
+    docs: {
+      page: SelectMultipleDocs,
+    },
+  },
+};
+
+export const grouping = () => (
+  <Wrapper>
+    <SelectGrouping />
+  </Wrapper>
+);
+grouping.story = {
+  parameters: {
+    status: 'Development', // Stable | Development | Deprecated
+    docs: {
+      page: SelectGroupingDocs,
     },
   },
 };
