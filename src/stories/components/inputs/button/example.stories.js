c276b262891c9ba77dd9cb050b101b041e02693e
Ngadiono CR <ngadionocr@gmail.com>
2021-01-01 22:14:23 +0700

    change ux docs

--- src/stories/components/inputs/button/example.stories.js
+++ src/stories/components/inputs/button/example.stories.js
@@ -9,29 +9,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import ButtonTypes from './button-types';
 import ButtonTypesDocs from './button-types/docs.mdx';
 
-import ButtonContained from './button-contained';
-import ButtonContainedDocs from './button-contained/docs.mdx';
-
-import ButtonText from './button-text';
-import ButtonTextDocs from './button-text/docs.mdx';
-
-import ButtonOutlined from './button-outlined';
-import ButtonOutlinedDocs from './button-outlined/docs.mdx';
-
 import ButtonSizes from './button-sizes';
 import ButtonSizesDocs from './button-sizes/docs.mdx';
 
 import ButtonIconsLabel from './button-icons-label';
 import ButtonIconsLabelDocs from './button-icons-label/docs.mdx';
-
-import ButtonIcons from './button-icons';
-import ButtonIconsDocs from './button-icons/docs.mdx';
-
-import ButtonLoading from './button-loading';
-import ButtonLoadingDocs from './button-loading/docs.mdx';
-
-import ButtonCustomized from './button-customized';
-import ButtonCustomizedDocs from './button-customized/docs.mdx';
 
 import ButtonFullWidth from './button-full-width';
 import ButtonFullWidthDocs from './button-full-width/docs.mdx';
@@ -65,58 +47,11 @@ export const types = () => (
   </Wrapper>
 );
 types.story = {
+  title: 'Type Buttons',
   parameters: {
     status: 'Stable', // Stable | Development | Deprecated
     docs: {
       page: ButtonTypesDocs,
-      source: {
-        code: 'Some custom string here',
-      },
-    },
-    options: { showPanel: false },
-  },
-};
-
-export const contained = () => (
-  <Wrapper>
-    <ButtonContained />
-  </Wrapper>
-);
-contained.story = {
-  parameters: {
-    status: 'Stable', // Stable | Development | Deprecated
-    docs: {
-      page: ButtonContainedDocs,
-    },
-    options: { showPanel: false },
-  },
-};
-
-export const text = () => (
-  <Wrapper>
-    <ButtonText />
-  </Wrapper>
-);
-text.story = {
-  parameters: {
-    status: 'Stable', // Stable | Development | Deprecated
-    docs: {
-      page: ButtonTextDocs,
-    },
-    options: { showPanel: false },
-  },
-};
-
-export const outlined = () => (
-  <Wrapper>
-    <ButtonOutlined />
-  </Wrapper>
-);
-outlined.story = {
-  parameters: {
-    status: 'Stable', // Stable | Development | Deprecated
-    docs: {
-      page: ButtonOutlinedDocs,
     },
     options: { showPanel: false },
   },
@@ -152,45 +87,6 @@ iconsLabel.story = {
     options: { showPanel: false },
   },
 };
-
-// export const icons = () => (
-//   <Wrapper>
-//     <ButtonIcons />
-//   </Wrapper>
-// );
-// icons.story = {
-//   parameters: {
-//     docs: {
-//       page: ButtonIconsDocs,
-//     },
-//   },
-// };
-
-// export const loading = () => (
-//   <Wrapper>
-//     <ButtonLoading />
-//   </Wrapper>
-// );
-// loading.story = {
-//   parameters: {
-//     docs: {
-//       page: ButtonLoadingDocs,
-//     },
-//   },
-// };
-
-// export const customized = () => (
-//   <Wrapper>
-//     <ButtonCustomized />
-//   </Wrapper>
-// );
-// customized.story = {
-//   parameters: {
-//     docs: {
-//       page: ButtonCustomizedDocs,
-//     },
-//   },
-// };
 
 export const fullWidth = () => (
   <Wrapper>
