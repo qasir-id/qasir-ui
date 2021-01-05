c8b39aafdd66d2c4baf3cc64838e6215bc991469
Ridho Abdul Majid <ridho@qasir.id>
2021-01-05 14:43:07 +0700

    modify custom chip, override with inline style

--- src/components/data-display/Chip/Chip.js
+++ src/components/data-display/Chip/Chip.js
@@ -3,15 +3,27 @@ import React from 'react';
 
 // Material UI
 import { ThemeProvider } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Chip from '@material-ui/core/Chip';
 
 import theme from '../../variables/theme';
 
+
+
 export default (props) => {
     const { customBackground, customFont } = props
+    
+    const useStyle = makeStyles({
+        custom: {
+            backgroundColor: customBackground,
+            color: customFont
+        }
+    })
+    const classes = useStyle();
+
     return (
         <ThemeProvider theme={theme}>
-            <Chip style={{backgroundColor: customBackground, color: customFont}} {...props}/>
+            <Chip style={props.style} className={classes.custom} {...props}/>
         </ThemeProvider>
     )
 }
