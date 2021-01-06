9f70db0428474376c897bbbf4c87a72a2a16362c
Ngadiono CR <ngadionocr@gmail.com>
2021-01-06 15:00:41 +0700

    update sidebar menu

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,3 +13,35 @@ addParameters({
 });
 
 addDecorator(withPerformance);
+
+// or global addParameters
+export const parameters = {
+  previewTabs: {
+    'storybook/docs/panel': {
+      index: 1,
+      title: 'Documentation',
+    },
+    canvas: { title: 'Code', hidden: false },
+  },
+  options: {
+    storySort: {
+      order: [
+        'Getting started',
+        [
+          'Intro',
+          'Installing Qasir UI',
+          'Using Qasir UI',
+          'Recommended libraries',
+          'Design resources',
+          'Browser support',
+          "What's new",
+          'Support and Feedback',
+          'Credits',
+        ],
+        'Documentation',
+        // 'Templates',
+        // 'Components',
+      ],
+    },
+  },
+};
--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,11 +1,37 @@
 import { create } from '@storybook/theming/create';
+import colors from './src/components/variables/colors';
 
 export default create({
   base: 'light',
 
+  // Colours
+  // colorPrimary: colors.red50,
+  // colorSecondary: colors.grey50,
+
+  // UI
+  // appBg: 'white',
+  // appContentBg: 'silver',
+  // appBorderColor: 'grey',
+  // appBorderRadius: 4,
+
   // Typography
   fontBase: '"Montserrat", sans-serif',
   fontCode: 'monospace',
+
+  // Text colors
+  // textColor: '#031c2d',
+  // headerCommon: { color: 'red' },
+
+  // Toolbar default and active colors
+  // barTextColor: 'silver',
+  // barSelectedColor: 'black',
+  // barBg: 'hotpink',
+
+  // Form colors
+  // inputBg: 'white',
+  // inputBorder: 'silver',
+  // inputTextColor: 'black',
+  // inputBorderRadius: 4,
 
   brandTitle: 'Qasir',
   brandUrl: 'https://www.qasir.id',
