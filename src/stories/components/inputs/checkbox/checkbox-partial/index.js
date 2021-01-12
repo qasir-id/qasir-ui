838bd40be93e9cf25e3511688e75db69e879b7ea
Ridho Abdul Majid <ridho@qasir.id>
2021-01-12 10:32:06 +0700

    add checkbox with form, checkbox partial design system qasir

--- src/stories/components/inputs/checkbox/checkbox-partial/index.js
+++ src/stories/components/inputs/checkbox/checkbox-partial/index.js
@@ -1,14 +1,22 @@
 // Vendors
 import React from 'react';
+import StopIcon from '@material-ui/icons/Stop';
 
 import CheckBox from 'components/inputs/Checkbox';
 
 
 const CheckboxPartial = (props) => {
-    
+    const styles = {
+       iconStyle: {
+           fill: '#f04b32',
+           border: '2px solid #DADCE5',
+           borderRadius: '2px',
+       }
+    }
     return(
         <>
-            <CheckBox 
+            <CheckBox
+                checkedIcon={<StopIcon fontSize="small" style={styles.iconStyle} />}
                 inputProps={{ 'aria-label': 'decorative checkbox' }}
             />
         </>
