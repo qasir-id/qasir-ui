16bbcf0e9ca13dddc1c0f6ff76701ff10323e8a8
Ngadiono CR <ngadionocr@gmail.com>
2021-01-19 12:08:19 +0700

    update icons

--- src/stories/components/data-display/icon/icon-qasir/index.js
+++ src/stories/components/data-display/icon/icon-qasir/index.js
@@ -1,10 +1,18 @@
 // Vendors
 import React, { useState, useEffect } from 'react';
+import { Source } from '@storybook/addon-docs/blocks';
+import dedent from 'ts-dedent';
 
 // Material UI
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
 
 // Components
 import TextField from 'components/inputs/TextField';
@@ -36,6 +44,21 @@ import IconInfo from 'components/data-display/Icon/IconInfo';
 import IconSort from 'components/data-display/Icon/IconSort';
 import IconSortAscending from 'components/data-display/Icon/IconSortAscending';
 import IconSortDescending from 'components/data-display/Icon/IconSortDescending';
+
+import colors from 'components/variables/colors';
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
 
 const useStyles = makeStyles((theme) => ({
   iconWrapper: {
@@ -75,6 +98,22 @@ const useStyles = makeStyles((theme) => ({
       lineHeight: '14px !important',
     },
   },
+  modalContent: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  modalIconPreview: {
+    '& svg': {
+      color: colors.red50,
+      fontSize: 210,
+      backgroundSize: '30px 30px',
+      backgroundColor: '#fff',
+      backgroundImage:
+        'linear-gradient(45deg, #f4f4f4 25%, transparent 25%), linear-gradient(-45deg, #f4f4f4 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #f4f4f4 75%), linear-gradient(-45deg, transparent 75%, #f4f4f4 75%)',
+      backgroundPosition: '0 0, 0 15px, 15px -15px, -15px 0',
+      marginRight: 20,
+    },
+  },
 }));
 
 function useDebounce(value, delay) {
@@ -94,38 +133,69 @@ function useDebounce(value, delay) {
 }
 
 const icons = [
-  { name: 'Dashboard', element: <IconDashboard /> },
-  { name: 'Report', element: <IconReport /> },
-  { name: 'Transaction', element: <IconTransaction /> },
-  { name: 'Product', element: <IconProduct /> },
-  { name: 'Staff', element: <IconStaff /> },
-  { name: 'Inventory', element: <IconInventory /> },
-  { name: 'Payment', element: <IconPayment /> },
-  { name: 'Outlet', element: <IconOutlet /> },
-  { name: 'Cashflow', element: <IconCashflow /> },
-  { name: 'Customer', element: <IconCustomer /> },
-  { name: 'Profile', element: <IconProfile /> },
-  { name: 'Calendar', element: <IconCalendar /> },
-  { name: 'Search', element: <IconSearch /> },
-  { name: 'Show', element: <IconShow /> },
-  { name: 'Hide', element: <IconHide /> },
-  { name: 'Expand', element: <IconExpand /> },
-  { name: 'Dropdown', element: <IconDropdown /> },
-  { name: 'Back', element: <IconBack /> },
-  { name: 'Exit', element: <IconExit /> },
-  { name: 'Setting', element: <IconSetting /> },
-  { name: 'Delete', element: <IconDelete /> },
-  { name: 'Close', element: <IconClose /> },
-  { name: 'Clear', element: <IconClear /> },
-  { name: 'Info', element: <IconInfo /> },
-  { name: 'Sort', element: <IconSort /> },
-  { name: 'Sort Ascending', element: <IconSortAscending /> },
-  { name: 'Sort Descending', element: <IconSortDescending /> },
+  { name: 'Dashboard', element: <IconDashboard />, importName: 'IconDashboard' },
+  { name: 'Report', element: <IconReport />, importName: 'IconReport' },
+  { name: 'Transaction', element: <IconTransaction />, importName: 'IconTransaction' },
+  { name: 'Product', element: <IconProduct />, importName: 'IconProduct' },
+  { name: 'Staff', element: <IconStaff />, importName: 'IconStaff' },
+  { name: 'Inventory', element: <IconInventory />, importName: 'IconInventory' },
+  { name: 'Payment', element: <IconPayment />, importName: 'IconPayment' },
+  { name: 'Outlet', element: <IconOutlet />, importName: 'IconOutlet' },
+  { name: 'Cashflow', element: <IconCashflow />, importName: 'IconCashflow' },
+  { name: 'Customer', element: <IconCustomer />, importName: 'IconCustomer' },
+  { name: 'Profile', element: <IconProfile />, importName: 'IconProfile' },
+  { name: 'Calendar', element: <IconCalendar />, importName: 'IconCalendar' },
+  { name: 'Search', element: <IconSearch />, importName: 'IconSearch' },
+  { name: 'Show', element: <IconShow />, importName: 'IconShow' },
+  { name: 'Hide', element: <IconHide />, importName: 'IconHide' },
+  { name: 'Expand', element: <IconExpand />, importName: 'IconExpand' },
+  { name: 'Dropdown', element: <IconDropdown />, importName: 'IconDropdown' },
+  { name: 'Back', element: <IconBack />, importName: 'IconBack' },
+  { name: 'Exit', element: <IconExit />, importName: 'IconExit' },
+  { name: 'Setting', element: <IconSetting />, importName: 'IconSetting' },
+  { name: 'Delete', element: <IconDelete />, importName: 'IconDelete' },
+  { name: 'Close', element: <IconClose />, importName: 'IconClose' },
+  { name: 'Clear', element: <IconClear />, importName: 'IconClear' },
+  { name: 'Info', element: <IconInfo />, importName: 'IconInfo' },
+  { name: 'Sort', element: <IconSort />, importName: 'IconSort' },
+  { name: 'Sort Ascending', element: <IconSortAscending />, importName: 'IconSortAscending' },
+  { name: 'Sort Descending', element: <IconSortDescending />, importName: 'IconSortDescending' },
 ];
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: '0 16px',
+    minWidth: 500,
+  },
+}))(MuiDialogContent);
 
 const IconQasir = () => {
   const classes = useStyles();
   const [iconState, setIconState] = useState(icons);
+  const [open, setOpen] = useState(false);
+  const [modalData, setModalData] = useState(null);
+
+  const handleClickOpen = (objIcon) => {
+    setOpen(true);
+    setModalData(objIcon);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   // Searching
   const [searchTerm, setSearchTerm] = useState(null);
@@ -163,8 +233,8 @@ const IconQasir = () => {
       <Grid container spacing={2} className={classes.iconWrapper}>
         <Grid item xs={12}>
           <Grid container justify="left" spacing={2}>
-            {iconState.map(({ name, element }, i) => (
-              <Grid item key={i}>
+            {iconState.map(({ name, element, importName }, i) => (
+              <Grid item key={i} onClick={() => handleClickOpen({ name, element, importName })}>
                 <Paper className={classes.iconItem}>
                   <div>
                     {element}
@@ -176,6 +246,23 @@ const IconQasir = () => {
           </Grid>
         </Grid>
       </Grid>
+
+      {modalData && (
+        <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+          <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+            {modalData.name}
+          </DialogTitle>
+          <DialogContent>
+            <div className={classes.modalContent}>
+              <div className={classes.modalIconPreview}>{modalData.element}</div>
+            </div>
+            <Source
+              language="jsx"
+              code={dedent`import ${modalData.importName} from 'qasir-ui/Icon/${modalData.importName}';`}
+            />
+          </DialogContent>
+        </Dialog>
+      )}
     </>
   );
 };
