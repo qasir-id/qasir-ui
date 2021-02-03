8b0b0760ed7378555a4d0ea763491fe8c13409d9
Ngadiono CR <ngadionocr@gmail.com>
2021-02-03 15:05:43 +0700

    adding description progress

--- src/stories/components/feedback/progress/circular/description.stories.js
+++ src/stories/components/feedback/progress/circular/description.stories.js
@@ -5,6 +5,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 // Components
+import ProgressCircularIndeterminate from './progress-circular-indeterminate';
+import ProgressCircularDeterminate from './progress-circular-determinate';
+import ProgressCircularInteractiveIntegration from './progress-circular-interactive-integration';
+import ProgressCircularWithLabel from './progress-circular-with-label';
+import ProgressCircularCustomized from './progress-circular-customized';
+
 import ProgressDescriptionDocs from './description.docs.mdx';
 
 const useStyles = makeStyles((theme) => ({
@@ -26,19 +32,36 @@ export default {
     design: { disabled: true },
     options: { showPanel: true },
   },
+  argTypes: {
+    type: {
+      name: 'Example',
+      control: {
+        type: 'select',
+        options: ['indeterminate', 'determinate', 'interactive integration', 'with label', 'customized'],
+      },
+    },
+  },
 };
 
 export const description = ({ type }) => (
   <Wrapper>
-    <h3 style={{ textAlign: 'center' }}>Under Construction</h3>
+    {type == 'indeterminate' && <ProgressCircularIndeterminate />}
+    {type == 'determinate' && <ProgressCircularDeterminate />}
+    {type == 'interactive integration' && <ProgressCircularInteractiveIntegration />}
+    {type == 'with label' && <ProgressCircularWithLabel />}
+    {type == 'customized' && <ProgressCircularCustomized />}
   </Wrapper>
 );
 
 description.story = {
   parameters: {
-    status: 'Development', // Stable | Development | Deprecated
+    status: 'Stable', // Stable | Development | Deprecated
     docs: {
       page: ProgressDescriptionDocs,
     },
   },
+};
+
+description.args = {
+  type: 'indeterminate',
 };
--- src/stories/components/feedback/progress/linear/description.stories.js
+++ src/stories/components/feedback/progress/linear/description.stories.js
@@ -5,6 +5,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 // Components
+import ProgressLinearIndeterminate from './progress-linear-indeterminate';
+import ProgressLinearDeterminate from './progress-linear-determinate';
+import ProgressLinearBuffer from './progress-linear-buffer';
+import ProgressLinearWithLabel from './progress-linear-with-label';
+import ProgressLinearCustomized from './progress-linear-customized';
+
 import ProgressDescriptionDocs from './description.docs.mdx';
 
 const useStyles = makeStyles((theme) => ({
@@ -26,19 +32,36 @@ export default {
     design: { disabled: true },
     options: { showPanel: true },
   },
+  argTypes: {
+    type: {
+      name: 'Example',
+      control: {
+        type: 'select',
+        options: ['indeterminate', 'determinate', 'buffer', 'with label', 'customized'],
+      },
+    },
+  },
 };
 
 export const description = ({ type }) => (
   <Wrapper>
-    <h3 style={{ textAlign: 'center' }}>Under Construction</h3>
+    {type == 'indeterminate' && <ProgressLinearIndeterminate />}
+    {type == 'determinate' && <ProgressLinearDeterminate />}
+    {type == 'buffer' && <ProgressLinearBuffer />}
+    {type == 'with label' && <ProgressLinearWithLabel />}
+    {type == 'customized' && <ProgressLinearCustomized />}
   </Wrapper>
 );
 
 description.story = {
   parameters: {
-    status: 'Development', // Stable | Development | Deprecated
+    status: 'Stable', // Stable | Development | Deprecated
     docs: {
       page: ProgressDescriptionDocs,
     },
   },
+};
+
+description.args = {
+  type: 'indeterminate',
 };
